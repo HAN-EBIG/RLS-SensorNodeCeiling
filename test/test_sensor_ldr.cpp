6ba023cbd32be104ce11f898deac8380c650c057
Jorn Brouwer <Jorn.brouwer@gmail.com>
2017-07-30 18:41:25 +0200

    added ldr test

--- test/test_sensor_ldr.cpp
+++ test/test_sensor_ldr.cpp
@@ -2,6 +2,12 @@
 #include <unity.h>
 
 #ifdef UNIT_TEST
+
+void test_interrupt_pin_1()
+{
+  TEST_ASSERT_EQUAL(expected, actual)
+}
+
 void test_led_builtin_pin_number(void)
 {
     TEST_ASSERT_EQUAL(LED_BUILTIN, 13);
